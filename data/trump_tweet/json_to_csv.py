9ae16ba55d48177213997729f5dfd299ca078e5b
Karun Kannan <Karun@Karuns-MBP.home>
2017-12-30 14:05:52 -0500

    Added 2015 tweets, and placed in CSV form

--- data/trump_tweet/json_to_csv.py
+++ data/trump_tweet/json_to_csv.py
@@ -3,11 +3,18 @@ import glob
 
 files = glob.glob('*.json')
 csv = open('trump_tweet_2016_2017.csv', 'w')
-i = 0
+print("Loading data into csv...")
+column_title = "id, text\n"
+csv.write(column_title)
 for x in files:
     cur = open(x, 'r')
     cur_file = json.load(cur)
     cur.close()
     for tweet in cur_file:
-        print(tweet['id_str'], i)
-        i += 1
+        id_str = tweet["id_str"]
+        text = "\"" + tweet["text"] + "\""
+        row = id_str + "," + text + "\n"
+        csv.write(row)
+
+csv.close()
+print("CSV loaded.")
