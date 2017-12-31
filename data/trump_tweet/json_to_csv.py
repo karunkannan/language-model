9492e8cfad01b18cacaa1a0871d39964f58695f1
Karun Kannan <karun.kannan15@gmail.com>
2017-12-31 16:04:21 -0500

    added 2014 json, csv updated

--- data/trump_tweet/json_to_csv.py
+++ data/trump_tweet/json_to_csv.py
@@ -5,7 +5,7 @@ import re
 files = glob.glob('*.json')
 csv = open('trump_tweet_2016_2017.csv', 'w')
 print("Loading data into csv...")
-column_title = "id, text\n"
+column_title = "text\n"
 csv.write(column_title)
 i = 0
 for x in files:
@@ -13,13 +13,12 @@ for x in files:
     cur_file = json.load(cur)
     cur.close()
     for tweet in cur_file:
-        id_str = tweet["id_str"].encode('utf-8')
         text = tweet["text"]
         matchObj = re.match('^RT|^"@', text)
         if matchObj != None:
             continue
-        text = "\"" + tweet["text"].encode('ascii', 'ignore') + "\""
-        row = id_str + "," + text + "\n"
+        text = "\"" + tweet["text"] + "\""
+        row = text + "\n"
         i += 1
         csv.write(row)
 
